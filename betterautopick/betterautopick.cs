f2e0f38f4ed3fb16757e429dcc35f7721d306752
kehrazy <100174039+kehrazy@users.noreply.github.com>
2022-06-08 10:30:36 +0300

    Добавьте файлы проекта.

---
+++ betterautopick/betterautopick.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+using Divine.Entity;
+using Divine.Entity.Entities.Abilities;
+using Divine.Entity.Entities.Abilities.Components;
+using Divine.Entity.Entities.Units.Heroes;
+using Divine.Entity.Entities.Units.Heroes.Components;
+using Divine.Service;
+
+namespace betterautopick
+{
+    public sealed class betterautopick : Bootstrapper
+    {
+        public Hero Me { get; set; }
+    }
+}
---
+++ betterautopick/menu_handler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using Divine.Entity.Entities.Units.Heroes.Components;
+using Divine.Input;
+using Divine.Menu.Items;
+using Divine.Numerics;
+using Divine.Renderer;
+
+namespace betterautopick
+{
+    public class menu_handler
+    {
+        public RootMenu BaseMenu { get; set; }
+
+        public menu_handler(betterautopick main)
+        {
+            Factory = Divine.Menu.MenuManager.CreateRootMenu("Techies Crappahilation");
+            Factory.SetFontColor(Color.Violet);
+        }
+}
